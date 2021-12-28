d98c75e0d3d4988cf504d9b091cd654753bad77a
Gwendal <Gwendal.lefloch82@gmail.com>
2021-12-28 23:24:11 +0100

    heroku

--- src/components/socket.js
+++ src/components/socket.js
@@ -1,5 +1,5 @@
 import socketio from "socket.io-client";
 import React from "react";
 
-export const socket = socketio.connect("http://localhost:8000");
+export const socket = socketio.connect("https://microgames.herokuapp.com/");
 export const SocketContext = React.createContext();
