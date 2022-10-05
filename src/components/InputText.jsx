b9f50a10a46f1a3abd76ec89e3571afdbe089097
Gwendal Le floch <gwendal.lefloch82@gmail.com>
2022-10-05 12:40:09 +0200

    logo + fix

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -17,7 +17,7 @@ class InputTxt extends React.Component {
         return (
             <div className={`ctn-input ${ this.props.col}`}>
                 <label htmlFor={this.props.id}>{this.props.label}</label>
-                <input type={this.state.type} className='m-0' maxLength="30" autoFocus required placeholder={this.props.letter + "..."} name={this.props.id} id={this.props.id} autoComplete="off"/>
+                <input type={this.state.type} className='m-0' maxLength="30" required placeholder={this.props.letter + "..."} name={this.props.id} id={this.props.id} autoComplete="off"/>
             </div>
         )  
     }
